0558e5dc0da1f1ccc4e8015d1de884e958f5b647
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-05-01 00:18:53 +0900

    최고 점수 기능 추가

--- heamin01/src/main/java/com/example/heamin01/service/ScoreService.java
+++ heamin01/src/main/java/com/example/heamin01/service/ScoreService.java
@@ -1,24 +1,28 @@
 package com.example.heamin01.service;
 
-import com.example.heamin01.dto.ApiResponse;
-import com.example.heamin01.dto.ErrorResponseDTO;
+import com.example.heamin01.util.ApiResponse;
+import com.example.heamin01.util.ErrorResponse;
 import com.example.heamin01.dto.scoreDto.ScoreRequestDTO;
 import com.example.heamin01.dto.scoreDto.ScoreResponseDTO;
 import com.example.heamin01.entity.Category;
 import com.example.heamin01.entity.Member;
 import com.example.heamin01.entity.Score;
 import com.example.heamin01.mapper.MemberMapper;
-import com.example.heamin01.mapper.MemberMapperImpl;
 import com.example.heamin01.repository.CategoryRepository;
 import com.example.heamin01.repository.MemberRepository;
 import com.example.heamin01.repository.ScoreRepository;
-import org.hibernate.annotations.NotFound;
+import com.example.heamin01.util.PageRequestDTO;
+import com.example.heamin01.util.PageResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Calendar;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -42,10 +46,10 @@ public class ScoreService {
         //member나 category가 존재하지 않을 경우 에러 객체 반환
         if (member.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(new ErrorResponseDTO("error", "결과 등록에 실패하였습니다.", "회원이 존재하지 않습니다."));}
+                    .body(new ErrorResponse("error", "결과 등록에 실패하였습니다.", "회원이 존재하지 않습니다."));}
             if (category.isEmpty()) {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(new ErrorResponseDTO("error", "결과 등록에 실패하였습니다.", "존재하지 않는 카테고리 입니다."));}
+                        .body(new ErrorResponse("error", "결과 등록에 실패하였습니다.", "존재하지 않는 카테고리 입니다."));}
 
                 //둘다 존재할 경우 dto -> entity 변환
                 Score score = dto.toEntity();
@@ -55,5 +59,20 @@ public class ScoreService {
                 scoreRepository.save(score);
                 //성공 응답 객체 반환
                 return ResponseEntity.ok(new ApiResponse<ScoreResponseDTO>(true, mapper.toScoreDto(score), "결과 등록에 성공하였습니다."));
+            }
+            public PageResponse<Score> userScoreList(HttpSession session,PageRequestDTO dto){
+                    //멤버의 게시물에서만 페이지를 조회해야함
+                    Pageable pageable = PageRequest.of(dto.getPage(), dto.getSize());
+                    Page<Score> scores = scoreRepository.findAllByMemberId((Long)session.getAttribute("loginID"),pageable);
+
+                    PageResponse<Score> response = new PageResponse<>(scores.getContent(),dto.getPage(),dto.getSize(),scores.getTotalElements(),scores.getTotalPages(),scores.hasPrevious()
+                    ,scores.hasNext());
+                    return response;
+            }
+
+            public ResponseEntity<?> userBestScore(HttpSession session){
+                List<Score> list = scoreRepository.findTopScore((Long)session.getAttribute("loginID"));
+                if(list.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("error","최고 점수 조회 실패","실전 문제를 푼 기록이 아직 없습니다."));
+                else return ResponseEntity.ok(mapper.toScoreDto(list.get(0)));
             }
         }
