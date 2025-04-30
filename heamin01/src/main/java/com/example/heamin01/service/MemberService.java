ccb2119d252f0388586d4350bdd80b79718f3215
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-05-01 00:20:19 +0900

    로그인 비밀번호 비교 에러 수정

--- heamin01/src/main/java/com/example/heamin01/service/MemberService.java
+++ heamin01/src/main/java/com/example/heamin01/service/MemberService.java
@@ -1,17 +1,14 @@
 package com.example.heamin01.service;
 
-import com.example.heamin01.dto.ErrorResponseDTO;
+import com.example.heamin01.util.ErrorResponse;
 import com.example.heamin01.dto.LoginRequestDTO;
 import com.example.heamin01.dto.memberDto.MemberRequestDTO;
 import com.example.heamin01.dto.memberDto.MemberResponseDTO;
 import com.example.heamin01.entity.Member;
 import com.example.heamin01.mapper.MemberMapper;
-import com.example.heamin01.mapper.MemberMapperImpl;
 import com.example.heamin01.repository.MemberRepository;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -49,9 +46,10 @@ public class MemberService {
 
     //로그인
     public ResponseEntity<?> login(HttpSession session,LoginRequestDTO dto){
-        Optional<Member> member = memberRepository.findByEmailAndPassword(dto.getEmail(),dto.getPassword());
+
+        Optional<Member> member = memberRepository.findByEmail(dto.getEmail());
         //member가 존재할 경우 LoginResponseDTO반환
-        if(member.isPresent()){
+        if(member.isPresent() && passwordEncoder.matches(dto.getPassword(),member.get().getPassword())){
             //로그인 성공 시 session에 데이터 저장
             session.setAttribute("loginID",member.get().getId());
             session.setAttribute("loginName",member.get().getName());
@@ -60,7 +58,7 @@ public class MemberService {
         }else{
             //ResponseEntity.status(HttpStatus.UNAUTHORIZED).body
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                    new ErrorResponseDTO("error","로그인에 실패하였습니다.","아이디 또는 비밀번호가 일치하지 않습니다.")
+                    new ErrorResponse("error","로그인에 실패하였습니다.","아이디 또는 비밀번호가 일치하지 않습니다.")
             );
         }
     }
@@ -87,7 +85,7 @@ public class MemberService {
         if(member.isPresent()){
             return ResponseEntity.ok(mapper.toMemberDto(member.get()));
         }else{
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDTO("not found","조회되는 회원이 없습니다.","조회 데이터 없음"));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("not found","조회되는 회원이 없습니다.","조회 데이터 없음"));
         }
     }
 
@@ -122,7 +120,7 @@ public class MemberService {
             //ResponseDTO 반환
             return ResponseEntity.ok(mapper.toMemberDto(memberRepository.findById(dto.getId()).get()));
         }else{
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDTO("404","수정에 실패하였습니다.","존재하지않는 회원이거나 유효성 검사 실패"));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("404","수정에 실패하였습니다.","존재하지않는 회원이거나 유효성 검사 실패"));
         }
     }
 }
