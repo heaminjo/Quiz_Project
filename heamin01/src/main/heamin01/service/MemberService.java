fb959ac881fec1087971c090181fb80b6f991311
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-05-06 23:26:50 +0900

    Add files via upload

--- heamin01/src/main/heamin01/service/MemberService.java
+++ heamin01/src/main/heamin01/service/MemberService.java
@@ -11,23 +11,26 @@ import com.example.heamin01.repository.MemberRepository;
 
 
 import com.example.heamin01.util.exception.IncorrectFormat;
+import com.example.heamin01.util.exception.NotFoundException;
 import com.example.heamin01.util.exception.PropertyException;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
 import org.hibernate.PropertyValueException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @Log4j2
@@ -52,68 +55,74 @@ public class MemberService {
         //암호화 변환
         dto.setPassword(passwordEncoder.encode(dto.getPassword()));
 
-
         if(dto.getName() == null){
             throw new PropertyException("필수값이 누락되었습니다.");
         }
         if (!dto.getEmail().matches("^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$")) {
             throw new IncorrectFormat("이메일의 형식이 잘못되었습니다.");
         }
-            if (!memberRepository.existsByEmail(dto.getEmail())) {
-                member = memberRepository.save(dto.toEntity());
+        if (!memberRepository.existsByEmail(dto.getEmail())) {
+            member = memberRepository.save(dto.toEntity());
 
 
-                log.info("회원가입 성공 => " + dto.getName());
-            } else {
-                log.info("회원가입 실패 => " + dto);
-            }
+            log.info("회원가입 성공 => " + dto.getName());
+        } else {
+            log.info("회원가입 실패 => " + dto);
+        }
         return ResponseEntity.ok(new ApiResponse<MemberResponseDTO>(true,mapper.toMemberDto(member),"회원 가입에 성공하셨습니다."));
     }
 
     //로그인
     public ResponseEntity<?> login(HttpSession session,LoginRequestDTO dto){
 
-        Optional<Member> member = memberRepository.findByEmail(dto.getEmail());
-        //member가 존재할 경우 LoginResponseDTO반환
-        if(member.isPresent() && passwordEncoder.matches(dto.getPassword(),member.get().getPassword())){
+        //아이디 검사
+        //조회 아이디 없을 시 예외 발생
+        Member member = memberRepository.findByEmail(dto.getEmail()).orElseThrow(()-> new NotFoundException("이메일 또는 비밀번호가 일치하지 않습니다."));
+
+        //비밀번호 검사
+        if(passwordEncoder.matches(dto.getPassword(),member.getPassword())){
             //로그인 성공 시 session에 데이터 저장
-            session.setAttribute("loginID",member.get().getId());
-            session.setAttribute("loginName",member.get().getName());
+            session.setAttribute("loginID",member.getId());
+            session.setAttribute("loginName",member.getName());
            
-            log.info("로그인 성공 => "+member.get().getId() + member.get().getName());
+            log.info("로그인 성공");
            
-            return ResponseEntity.ok(mapper.toLoginDto(member.get()));
+            return ResponseEntity.ok(mapper.toLoginDto(member));
         }else{
             //ResponseEntity.status(HttpStatus.UNAUTHORIZED).body
-            log.info("로그인 실패 => 존재하지 않는 아이디 혹은 비밀번호 ");
+            log.info("로그인 실패");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                    new ErrorResponse(HttpStatus.BAD_REQUEST,"아이디 또는 비밀번호가 일치하지 않습니다.","..")
+                    new ErrorResponse(HttpStatus.BAD_REQUEST,"이메일 또는 비밀번호가 일치하지 않습니다.","..")
             );
         }
     }
 
     //전체 조회
     public List<MemberResponseDTO> listAll(){
-        List<MemberResponseDTO> list = new ArrayList<>();
+        //회원의 모든 데이터를 가져온다.
         List<Member> memberList = memberRepository.findAll();
 
-        //DTO로 변환 하여 저장
+        //DTO로 변환 하여 저장하는 메서드 구현
         Function<Member,MemberResponseDTO> fn = (m) -> mapper.toMemberDto(m);
-        list = memberList.stream().map(fn).toList();
-        list.forEach(i->System.out.println(i.getName()));
+        //Stream을 통해 map을 돌려 DB에서 가져온 memberList를 순회하며 DTO로 변환 후 List로 생성
+        List<MemberResponseDTO> list = memberList.stream().map(fn).toList();
         return list;
     }
 
     //회원 조회
     public ResponseEntity<?> selectMember(Long id){
-        Optional<Member> member = memberRepository.findById(id);
 
-        //조회되는 회원이 있는 경우 MemberResponseDTO 반환
-        if(member.isPresent()){
-            return ResponseEntity.ok(mapper.toMemberDto(member.get()));
-        }else{
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(HttpStatus.NOT_FOUND,"조회되는 회원이 없습니다.","조회 데이터 없음"));
-        }
+
+        Optional<Member> member = memberRepository.findById(id);
+        //자바 컴파일러의 자동 추론으로 의해 Optional::isPresent -> (member) -> member.isPresent()로 변환된다.
+        //Optional을 받아 isPresent() 즉 값이 들어있는지 검사를 하기 위한 함수 인터페이스를 작성
+        Predicate<Optional<Member>> predicate = Optional::isPresent;
+
+        return predicate.test(member) ?
+                ResponseEntity.ok(mapper.toMemberDto(member.get())):
+                ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    new ErrorResponse(HttpStatus.NOT_FOUND,"조회되는 회원이 없습니다.","404"));
+
     }
 
     //파일업로드
@@ -140,14 +149,14 @@ public class MemberService {
     }
 
     //회원 수정
+    //엔티티 변경 시 자동으로 save()
+    @Transactional
     public ResponseEntity<?> memberUpdate(MemberRequestDTO dto){
-        //멤버가 있는지 확인
-        if(memberRepository.existsById(dto.getId())){
-            memberRepository.save(dto.toEntity());
-            //ResponseDTO 반환
-            return ResponseEntity.ok(mapper.toMemberDto(memberRepository.findById(dto.getId()).get()));
-        }else{
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(HttpStatus.NOT_FOUND,"수정에 실패하였습니다.","존재하지않는 회원이거나 유효성 검사 실패"));
-        }
+
+        Member member = memberRepository.findById(dto.getId()).orElseThrow(()-> new NotFoundException("존재하지 않는 회원입니다."));
+        member.setName(dto.getName());
+        member.setBirth(dto.getBirth());
+
+        return ResponseEntity.ok(new ApiResponse<>(true,mapper.toMemberDto(member),"회원 수정을 성공하였습니다."));
     }
 }
--- heamin01/src/main/heamin01/service/QuizService.java
+++ heamin01/src/main/heamin01/service/QuizService.java
@@ -1,16 +1,19 @@
 package com.example.heamin01.service;
 
 import com.example.heamin01.dto.QuizDTO;
+import com.example.heamin01.entity.Category;
 import com.example.heamin01.entity.Quiz;
 import com.example.heamin01.mapper.MemberMapper;
 import com.example.heamin01.repository.QuizRepository;
 import com.example.heamin01.util.ErrorResponse;
+import com.example.heamin01.util.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.function.Function;
 
 @Service
 public class QuizService {
@@ -24,23 +27,23 @@ public class QuizService {
     public ResponseEntity<?> quizList(Long category){
         Random rand = new Random();
         Set<Integer> set = new HashSet<>();
-        List<Quiz> list;
+        List<Quiz> list= new ArrayList<>();
+
+        if(quizRepository.existsById(category)) throw new NotFoundException("존재하지 않는 카테고리 입니다.");
 
         if(category == 0){
+            //중복되지않게 set에 난수
             while(set.size() <20){
                 set.add(rand.nextInt(90)+1);}
             list = quizRepository.findByQuiz(set);
         }else if(category >=1 && category <=9){
             list = quizRepository.findByCategory_CategoryId(category);
             Collections.shuffle(list);
-        }else{
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("error","존재하지 않는 카테고리","카테고리를 다시 선택해주세요"));}
-
-        List<QuizDTO> response = new ArrayList<>();
-
-        for(Quiz q:list){
-            response.add(new QuizDTO(q.getQuestion(),q.getResult(),q.getCategory().getName()));
         }
+        //퀴즈를 받아 dto로 변환하는 인터페이스
+        Function<Quiz,QuizDTO> fn = (quiz) -> mapper.toQuizDto(quiz);
+        List<QuizDTO> response = list.stream().map(fn).toList();
+
         return ResponseEntity.ok(response);
     }
 }
